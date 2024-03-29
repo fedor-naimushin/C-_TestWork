6cbf5fc4860370b1a039dc76d47b8387283e179a
Naimushin Fedor <fedor.naimushin@urfu.me>
2024-03-29 18:02:56 +0500

    Use Swagger

--- UssJuniorTest/Program.cs
+++ UssJuniorTest/Program.cs
@@ -4,8 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.RegisterAppServices();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 app.UseRouting();
